56ac6a76c2b0b5e03535ffb5c807eac7ed7e0222
magcimarcin1234 <skulimowskim1990@gmail.com>
2018-02-05 21:31:01 +0100

    no message

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE HTML>
+<html lang ="pl">
+
+<head>
+    <meta charset="UTF-8"/>
+    <title>Moja stronka</title>
+
+
+    <link rel="stylesheet" href="style.css" type="text/css" />
+
+
+
+
+</head>
+
+<body>
+<div id="contaier" >
+
+    <div id="header">
+
+        <div id="headerL">
+            <img src="image.png">
+        </div>
+        <div id="title"><span style="color: #8C1717"> chudnij </span><span style="color: #DB70DB">ze mną!!!!</span>
+        </div>
+
+        <div style ="clear:both;" ></div>
+</div>
+
+    <div id="menu">
+        <div class ="option">Dieta</div>
+        <div class ="option">Cwiczenia</div>
+        <div class ="option">Trening</div>
+        <div class ="option">Rozrywka</div>
+        <div style ="clear:both;" ></div>
+    </div>
+
+    <div id="slider" >
+    miejsce na slider
+    </div>
+     <div id ="content">
+        <br/>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pulvinar justo vel purus posuere, eget hendrerit mi gravida. Donec ultricies varius lectus at pulvinar. Donec neque ligula, laoreet sed feugiat a, suscipit sit amet sem. Aenean vel erat eu nisi fringilla gravida quis non ipsum. Nulla eu nunc ac mi fringilla facilisis in vitae magna. Vivamus fermentum dolor ac commodo tincidunt. Praesent fermentum dolor in lectus sodales feugiat. Aliquam dignissim, dui eget elementum volutpat, metus tortor elementum sapien, et semper mauris nisl quis dui. Aenean semper ornare leo, ut rutrum enim. Quisque sit amet lobortis dui. Vivamus laoreet accumsan mollis. Quisque mauris ex, iaculis eu tristique eu, ornare quis urna. Donec viverra elementum sem, nec consectetur nisi sodales at. Donec sit amet tellus vel massa fringilla pharetra id in massa. Curabitur quis rhoncus metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+
+         <br/><br/>
+
+         Fusce quis dignissim purus. Proin sagittis tincidunt porta. Vivamus eu eros ut velit elementum convallis in non mauris. Aliquam nec eleifend est. Vivamus sed leo mi. Aliquam erat volutpat. Pellentesque auctor felis metus, nec maximus ante faucibus ut. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur vitae dolor ut quam vehicula mattis. Ut sed congue tortor. Maecenas auctor feugiat purus ut efficitur.
+     </div>
+
+
+    <div id="movies"> elegancka piosenka
+        <iframe width="346" height="199" src="https://www.youtube.com/embed/2iUGfURqyhs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+
+    <div id="links">
+        <div class="social">tutaj beda linki do portali spolecznosciowych</div>
+
+            <div class ="socialdivs">
+
+                    <div class="fb" >fb</div>
+                    <div class="yt" >yt</div>
+
+                    <div style="clear:both"></div>
+
+
+            </div>
+
+    </div>
+    <div style ="clear:both;" ></div>
+
+
+    <div id="footer">
+        &copy; Wszelkie prawa zastrzezone.
+    </div>
+
+
+
+
+</div>
+
+
+
+</body>
+
+
+
+
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
